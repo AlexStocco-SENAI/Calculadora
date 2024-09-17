f4ca9834b42bd353d080cc1ebf97969a87ae843d
Alex Stocco <alexstocco.senai@gmail.com>
2024-09-17 08:55:07 -0300

    Montagem da tela de splash

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            progressBar1 = new ProgressBar();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(29, 109);
+            label1.Name = "label1";
+            label1.Size = new Size(202, 37);
+            label1.TabIndex = 0;
+            label1.Text = "CARREGANDO";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(76, 158);
+            label2.Name = "label2";
+            label2.Size = new Size(102, 25);
+            label2.TabIndex = 1;
+            label2.Text = "AGUARDE";
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(12, 268);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(651, 29);
+            progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.RoyalBlue;
+            BackgroundImage = Properties.Resources.FundoSplash;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(675, 309);
+            Controls.Add(progressBar1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            DoubleBuffered = true;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Calculadora 3000";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private ProgressBar progressBar1;
     }
 }
